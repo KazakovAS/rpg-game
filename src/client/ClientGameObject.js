96001dc41df63b77e6fe76d166e013b3809afee3
Alexey Kazakov <a.kazakov@betaonline.ru>
2021-07-26 17:45:17 +0300

    test

--- src/client/ClientGameObject.js
+++ src/client/ClientGameObject.js
@@ -1,4 +1,5 @@
 import MovableObject from '../common/MovableObject';
+import { animateEx } from '../common/util';
 
 class ClientGameObject extends MovableObject {
   constructor(cfg) {
@@ -27,6 +28,8 @@ class ClientGameObject extends MovableObject {
         objectConfig: objCfg,
         type: objCfg.type,
         world,
+        state: 'main',
+        animationStartTime: 0,
       },
       cfg,
     );
@@ -34,7 +37,7 @@ class ClientGameObject extends MovableObject {
 
   moveByCellCoord(dcol, drow, conditionCallback = null) {
     const { cell } = this;
-    this.moveToCellCoord(
+    return this.moveToCellCoord(
       cell.cellCol + dcol,
       cell.cellRow + drow,
       conditionCallback,
@@ -44,8 +47,13 @@ class ClientGameObject extends MovableObject {
   moveToCellCoord(dcol, drow, conditionCallback = null) {
     const { world } = this;
     const newCell = world.cellAt(dcol, drow);
+    const canMove = !conditionCallback || conditionCallback(newCell);
 
-    if (!conditionCallback || conditionCallback(newCell)) this.setCell(newCell);
+    if (canMove) {
+      this.setCell(newCell);
+    }
+
+    return canMove;
   }
 
   setCell(newCell) {
@@ -60,15 +68,32 @@ class ClientGameObject extends MovableObject {
     }
   }
 
+  setState(state) {
+    this.state = state;
+
+    if (this.world) {
+      this.animationStartTime = this.world.engine.lastRenderTime;
+    }
+  }
+
+  getCurrentFrame(time) {
+    const state = this.spriteCfg.states[this.state];
+    const lengthFrame = state.frames.length;
+    const animate = animateEx(lengthFrame, this.animationStartTime, time, state.duration, true);
+    const frame = (lengthFrame + animate.offset | 0) % lengthFrame;
+
+    return state.frames[frame];
+  }
+
   render(time) {
     super.render(time);
 
     const { x, y, width, height, world } = this;
     const engine = world.engine;
 
-    const { sprite, frame, states } = this.spriteCfg;
+    const { sprite, frame, states, type } = this.spriteCfg;
 
-    const spriteFrame = states ? states.main.frames[0] : frame;
+    const spriteFrame = type === 'static' ? frame : this.getCurrentFrame(time);
 
     engine.renderSpriteFrame({
       sprite,
